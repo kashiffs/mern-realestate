4167217cd8898c10bab964597bcab0ddb516a7c9
Kashif <kashifsiddiqui309@gmail.com>
2025-01-15 22:06:23 +0530

    create signup api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test } from '../controlers/user.controler.js';
+import { test } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
