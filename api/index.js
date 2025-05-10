2003feacbfd1b49ceccec709864ebf7982a9005d
Kashif <kashifsiddiqui309@gmail.com>
2025-05-10 21:14:42 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -12,6 +14,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
 });
 
 const app = express();
+
+app.use(cookieParser());
 
 app.use(express.json());
 
