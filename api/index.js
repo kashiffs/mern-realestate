2a9f595a574a23d3d64f39f7e9f2aea7b479affa
Kashif <kashifsiddiqui309@gmail.com>
2025-05-13 21:26:00 +0530

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -15,9 +16,10 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app = express();
 
+app.use(express.json());
+
 app.use(cookieParser());
 
-app.use(express.json());
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
@@ -25,6 +27,8 @@ app.listen(3000, ()=>{
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
